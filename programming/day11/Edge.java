2f26dd3241363f8bd9533a9da22c773107a0c8eb
Kon Aoki <kon.aoki.us@gmail.com>
2017-12-13 17:08:50 -0700

    interfaces implemented

---
+++ programming/day11/Edge.java
@@ -0,0 +1,55 @@
+/**
+* Interface for an edge in a graph. Edges are immutable.
+*/
+public class Edge<N,W> implements IEdge<N,W>{
+  INode<N> source;
+  INode<N> destination;
+  W weight;
+
+  // Implementors should implement a construct that takes in the
+  // source, destination, and weight
+  public Edge(N source, N destination, W weight)
+  {
+    this.source=source;
+    this.destination=destination;
+    this.weight=weight;
+  }
+  /**
+  * The source node of the edge
+  * @return the source node
+  */
+  public INode<N> getSource()
+  {
+    return source;
+  }
+
+  /**
+  * The destination node of the edge
+  * @return the destination node
+  */
+  public INode<N> getDestination()
+  {
+    return destination;
+  }
+
+  /**
+  * The weight of the edge
+  * @return the weight
+  */
+  public W getWeight()
+  {
+    return weight;
+  }
+
+  /**
+  * Test for equality of two edges.
+  * Edges are equal when the node instances are exactly the same; i.e. this.src==o.src
+  * and this.dst == o.dst
+  * @param o the other edge
+  * @return true if the edges are the same
+  */
+  public boolean equals(Object o)
+  {
+    return (source==o.source && destination==o.destination)?true:false;
+  }
+}
